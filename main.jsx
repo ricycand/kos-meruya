975c285893b47b6e32876a3df46c4ef7199c98f7
ricycand <ricycandra.prius@gmail.com>
2026-05-21 14:45:21 +0700

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,8 +1,4 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
-// Stable wrapper prevents React removeChild errors
-const wrapper = document.createElement('div')
-document.getElementById('root').appendChild(wrapper)
-createRoot(wrapper).render(<App />)
+createRoot(document.getElementById('root')).render(<App />)
